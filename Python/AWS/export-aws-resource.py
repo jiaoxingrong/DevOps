3ec7476cc00d3421d7642fa6bb8426fd0a7f6771
Jerome <jiaoxingrong@oasgames.com>
2017-01-03 11:56:13 +0800

    *

--- Python/AWS/export-aws-resource.py
+++ Python/AWS/export-aws-resource.py
@@ -16,6 +16,7 @@ sys.setdefaultencoding('GBK')
 region_contrast = {'ap-northeast-2': 'Asia Pacific (Seoul)', 'ap-south-1': 'Asia Pacific (Mumbai)', 'sa-east-1': 'South America (Sao Paulo)', 'eu-west-1': 'EU (Ireland)', 'eu-central-1': 'EU (Frankfurt)', 'ap-southeast-1': 'Asia Pacific (Singapore)', 'ap-southeast-2': 'Asia Pacific (Sydney)', 'us-west-1': 'US West (N. California)', 'ap-northeast-1': 'Asia Pacific (Tokyo)', 'us-west-2': 'US West (Oregon)', 'us-east-1': 'US East (N. Virginia)', 'us-east-2': 'US East (Ohio)'}
 
 def cal_run_hours(cal_date,compare_date=datetime.datetime.utcnow().strftime('%Y%m')):
+    print cal_date,compare_date
     compare_date = str(compare_date)
     if int(compare_date[-2:]) == 1:
         compare_date_year = int(compare_date[:4]) - 1
