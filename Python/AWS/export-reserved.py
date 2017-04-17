fe04b11aed0d1341a41bb9510d640d3ec64b8304
Jerome <jiaoxingrong@oasgames.com>
2017-04-17 14:29:04 +0800

    improve aws util export script

--- Python/AWS/export-reserved.py
+++ Python/AWS/export-reserved.py
@@ -70,8 +70,7 @@ def cachereserve(session, report_file, region_name, profile):
 
 
 def main(profile):
-    # regions = ['eu-west-1','ap-southeast-1','ap-southeast-2','eu-central-1','ap-northeast-2','ap-northeast-1','us-east-1','sa-east-1','us-west-1','us-west-2']
-    regions = ['us-east-1']
+    regions = ['eu-west-1','ap-southeast-1','ap-southeast-2','eu-central-1','ap-northeast-2','ap-northeast-1','us-east-1','sa-east-1','us-west-1','us-west-2']
     region_contrast = {
     'ap-northeast-2': 'Asia Pacific (Seoul)', 'ap-south-1': 'Asia Pacific (Mumbai)', 'sa-east-1': 'South America (Sao Paulo)', 'eu-west-1': 'EU (Ireland)', 'eu-central-1': 'EU (Frankfurt)', 'ap-southeast-1': 'Asia Pacific (Singapore)', 'ap-southeast-2': 'Asia Pacific (Sydney)', 'us-west-1': 'US West (N. California)', 'ap-northeast-1': 'Asia Pacific (Tokyo)', 'us-west-2': 'US West (Oregon)', 'us-east-1': 'US East (N. Virginia)', 'us-east-2': 'US East (Ohio)'
     }
